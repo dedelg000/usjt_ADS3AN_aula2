97b634257d0e1d0deb45af887d3af1a9806082e9
dedelg000 <daniel.leme000@gmail.com>
2019-08-29 18:31:42 -0300

    Initial commit

---
+++ app/src/main/java/ads3an/usjt/com/usjt_ads3an_aula2/MainActivity.java
@@ -0,0 +1,30 @@
+package ads3an.usjt.com.usjt_ads3an_aula2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public final static String EXTRA_MESSAGE = "br.usjt.meuprimeiroapp.MESSAGE";
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+
+
+}
---
+++ app/src/test/java/ads3an/usjt/com/usjt_ads3an_aula2/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package ads3an.usjt.com.usjt_ads3an_aula2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
